f7df259bfabbe867e331086732f26d0a8cf46f8a
RAHUL <rahulmeetawa@gmail.com>
2023-03-25 20:03:29 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import Expensecloud from "./components/Expense";
+
+const Container = styled.div`
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: column;
+  margin: 0 10px;
+  align-items: center;
+  height: 100vh;
+  width: 98%;
+  padding-top: 30px;
+  font-family: Montserrat;
+`;
+
+const Header = styled.div`
+  background-color: white;
+  color: #0d1d2c;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 22px;
+  font-size: 25px;
+  font-weight: bold;
+`;
+const App = () => {
+  return (
+    <Container>
+      <Header>Expense Tracker</Header>
+      <Expensecloud />
+    </Container>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Expensecloud from "./components/Expensecloud";
+import App from "./App";
+
 import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Expensecloud />
+    <App />
   </React.StrictMode>
 );
